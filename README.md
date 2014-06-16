976bf794dc1ad936ff9f82cd857f4c3ebbb967ef
Roger <rogerdammit@gmail.com>
2014-06-15 20:42:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+A Python library to download data from [flightradar24.com](http://www.flightradar24.com/).
+
+Supports downloading both archived ("playback") as well as "real-time" data, and can convert
+all units to SI.
+
+# Note
+
+This uses an unofficial API that can (and has) change at any moment, so try not to abuse it.
+Also there are two unknown fields (`unknown1` and `unknown2`) which don't seem important, but
+are still returned.
+
+# Usage
+
+Sample usage:
+
+```python
+from flightradar import get_current_data
+
+zone = 'full'
+
+data = get_current_data(zone) # downloads current data for every flight in the world!
+
+print data[0] # ranges in the thousands (e.g. 7,000+) so just print the first flight
+```
+
+gives:
+
+```python
+{'src': 'HGH', 'squawk': '1306', 'track': 253, 'type': 'A333', 'icao_addr': '78012D', 'reg_num': 'B-HLJ', 'long': 115.46, 'unknown2': 0, 'dest': 'HKG', 'radar': 'T-VHHH21', 'unknown1': 0, 'callsign': 'HDA623', 'time': datetime.datetime(2014, 6, 15, 20, 35, 23), 'flight_num': 'KA623', 'lat': 22.62, 'alt': 8092.4400000000005, 'time_epoch': 1402882523, 'speed': 240.759999792, 'id': '395791a', 'vert_speed': -12.354560000000001}
+```
+
+A list of zones can be found [here](http://www.flightradar24.com/js/zones.js.php).
+
+The `get_historical_data` also supports a callback function which will be passed the above dictionary for every record.
---
+++ flightradar24.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import requests
+import json
+import datetime
+
+
+__all__ = ['get_historical_data', 'get_last_data', 'get_current_data', \
+           'TIME_RES_HISTORICAL', 'TIME_RES_CURRENT']
+
+FIELDS = 'icao_addr', 'lat', 'long', 'track', 'alt', 'speed', 'squawk', 'radar', \
+         'type', 'reg_num', 'time_epoch', 'src', 'dest', 'flight_num', 'unknown1', \
+         'vert_speed', 'callsign', 'unknown2'
+# unknown1 sometimes == '1', usually == '0'
+
+URL_JSON_CURRENT = "http://db8.flightradar24.com/zones/{zone_name}.js"
+URL_JSON_HISTORICAL = 'http://db.flightradar24.com/playback/%Y%m%d/%H%M00.js'
+
+TIME_RES_HISTORICAL = datetime.timedelta(minutes=1)
+TIME_RES_CURRENT = datetime.timedelta(seconds=12) # estimated
+
+# multiplicative factors to convert to SI units
+CONV_KT_TO_MPS = 0.514444444
+CONV_FPM_TO_MPS = 5.08e-3
+CONV_FT_TO_M = 0.3048
+
+
+def get_historical_data(date_start, date_stop, step=TIME_RES_HISTORICAL, callback=None):
+    if date_stop < date_start:
+        raise Exception('stop date is before start date')
+
+    data = []
+    date_cur = date_start
+
+    while date_cur < date_stop:
+        url = date_cur.strftime(URL_JSON_HISTORICAL)
+        data_cur = load_data(url)
+
+        if callback:
+            callback(data_cur)
+        else:
+            data.extend(data_cur)
+
+        date_cur += step
+
+    return data
+
+def get_last_data(time_span, step=TIME_RES_HISTORICAL, callback=None):
+    stop = datetime.datetime.now()
+    start = stop - time_span
+
+    return get_historical_data(start, stop, step=step, callback=callback)
+
+def get_current_data(zone_name='full', use_faa=True):
+    if use_faa and not zone_name.endswith('_all'):
+        zone_name += '_all'
+
+    url = URL_JSON_CURRENT.format(zone_name=zone_name)
+
+    return load_data(url)
+
+def load_data(url, convert=True):
+    content = requests.get(url).content
+
+    # delete JS to make text valid JSON
+    content = content.replace('pd_callback(', '')
+    content = content.replace('fetch_playback_cb(', '')
+    content = content.replace(');', '')
+
+    # replace emply strings so Python gives us None's
+    content = content.replace('""', 'null')
+
+    json_data = json.loads(content)
+
+    # delete unused fields
+    version = json_data.pop('version')
+    full_count = json_data.pop('full_count')
+
+    data = []
+
+    for identifier, fields in json_data.items():
+        # convert unicode to strings to avoid issues later on
+        fields = [f.encode('utf-8') if isinstance(f, unicode) else f for f in fields]
+
+        f = dict(zip(FIELDS, fields))
+        f['id'] = identifier.encode('utf-8')
+
+        # convert units to SI
+        if convert:
+            f['time'] = datetime.datetime.fromtimestamp(f['time_epoch'])
+            f['speed'] *= CONV_KT_TO_MPS
+            f['alt'] *= CONV_FT_TO_M
+            f['vert_speed'] *= CONV_FPM_TO_MPS
+
+        data.append(f)
+
+    return data
+
+
+def main():
+    zone = 'full'
+
+    data = get_current_data(zone)
+
+    print data[0]
+    print len(data)
+
+
+if __name__ == '__main__':
+    main()
